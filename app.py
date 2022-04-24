ecc63c2b9ca743a06558d656c218f1178aef74b1
vnardini <vnardini@salesforce.com>
2022-04-24 14:08:35 +0100

    UPDATE 1

--- app.py
+++ app.py
@@ -2,67 +2,17 @@ import os
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-# Initializes your app with your bot token and signing secret
+# Initializes your app with your bot token and socket mode handler
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
 
+# Listens to incoming messages that contain "hello"
 @app.message("hello")
 def message_hello(message, say):
-    say(f"Hey there <@{message['user']}>")
-
-
-@app.event("app_home_opened")
-def update_home_tab(client, event, logger):
-    try:
-        # views.publish is the method that your app uses to push a view to the Home tab
-        client.views_publish(
-            # the user that opened your app's app home
-            user_id=event["user"],
-            # the view object that appears in the app home
-            view={
-                "type": "home",
-                "callback_id": "home_view",
-
-                # body of the view
-                "blocks": [
-                    {
-                        "type": "section",
-                        "text": {
-                            "type": "mrkdwn",
-                            "text": "*Welcome to your _App's Home_* :tada:"
-                        }
-                    },
-                    {
-                        "type": "divider"
-                    },
-                    {
-                        "type": "section",
-                        "text": {
-                            "type": "mrkdwn",
-                            "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
-                        }
-                    },
-                    {
-                        "type": "actions",
-                        "elements": [
-                            {
-                                "type": "button",
-                                "text": {
-                                    "type": "plain_text",
-                                    "text": "Click me!"
-                                }
-                            }
-                        ]
-                    }
-                ]
-            }
-        )
-
-    except Exception as e:
-        logger.error(f"Error publishing home tab: {e}")
+    # say() sends a message to the channel where the event was triggered
+    say(f"Hey there <@{message['user']}>!")
 
 
 # Start your app
 if __name__ == "__main__":
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
-    #signing_secret = os.environ.get("SLACK_SIGNING_SECRET")
